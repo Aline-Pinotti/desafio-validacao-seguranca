e04f7edd38c66876b7bbba1f7bb75a258d85a6c5
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-04-06 11:21:50 -0300

    ADD Services: City, Event - findAll, insert

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,14 +1,35 @@
 package com.devsuperior.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.CityRepository;
 import com.devsuperior.demo.repositories.EventRepository;
 
 @Service
 public class EventService {
 
     @Autowired
-    private EventRepository eventRepository;
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAll(Pageable pageable) {
+        return repository.findAll(pageable).map(EventDTO::new);
+    }
+
+    @Transactional
+    public EventDTO insert (EventDTO dto) {
+        Event entity = new Event(null, dto.getName(), dto.getDate(), dto.getUrl(), cityRepository.getReferenceById(dto.getCityId()));
+        return new EventDTO(entity);
+    }
+
 
 }
